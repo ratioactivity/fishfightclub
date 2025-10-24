0471856036de5975dfb420696e6582f885be4364
ratioactivity <semmatron@gmail.com>
2025-10-24 03:20:07 -0500

    Fix inventory tooltips and single victory loot

---
+++ items.js
@@ -0,0 +1,192 @@
+export const CUSTOM_ITEM_DATA = {
+  american_cheese: {
+    description: "100% American. Processed to Perfection.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  angler: {
+    description: "Yikes! Shouldn't have released that...",
+    useType: "KU",
+    useInfo: "Kills the first fish you click after redeeming.",
+  },
+  bacon: {
+    description: "A nutritious way to start your morning.",
+    useType: "HU",
+    useInfo: "Adds +10 power to the first fish you click after redeeming, but the benefit is only revealed afterward.",
+  },
+  barbeque_sauce: {
+    description: "It's mid.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  batteries: {
+    description: "ZZZZZZZZZZZZZZZZZ",
+    useType: "KU",
+    useInfo: "Immediately adds +10 power to every fish in your tank when redeemed.",
+  },
+  bell_pepper: {
+    description: "Spiciest Pepper for the Biggest Pussies.",
+    useType: "HU",
+    useInfo: "Secretly subtracts 10 power from the weakest fish in your tank.",
+  },
+  body_lotion: {
+    description: "Oh, oh yeah. That's nice.",
+    useType: "HU",
+    useInfo: "Causes a random fish to have an asexual child after redeeming.",
+  },
+  bubble_gum: {
+    description: "Gum?",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  butter: {
+    description: "Extra Creamy.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  cabbage: {
+    description: "Get the Cabbage on 3...",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  candy_bar: {
+    description: "If it's not Crunch, you're doing it wrong.",
+    useType: "HU",
+    useInfo: "Opens a popup asking if it's Crunch; answering no causes one of your fish to die.",
+  },
+  cereal: {
+    description: "Mmammfmmmm luckmy chrsm",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  coffee_bag: {
+    description: "10 shots of espresso please.",
+    useType: "HU",
+    useInfo: "Quietly adds +10 power to every fish in your tank when redeemed.",
+  },
+  cookies: {
+    description: "Hope you're not allergic to peanut butter.",
+    useType: "HU",
+    useInfo: "Shows a popup about allergies; admitting an allergy causes all of your fish to die.",
+  },
+  credit_card: {
+    description: "AYOOOOOOOOOOO",
+    useType: "HU",
+    useInfo: "Silently gives a random fish a fake $1,000,000 boost and the Super Rich trait.",
+  },
+  detergent: {
+    description: "Washy washy, happy happy!",
+    useType: "KU",
+    useInfo: "Displays a message that all fish are clean, with no other effect.",
+  },
+  egg: {
+    description: "YOU FUCKING BROKE THE YOLK YOU PIECE OF SHIT",
+    useType: "HU",
+    useInfo: "Makes every fish play the death animation without anyone actually dying.",
+  },
+  eraser: {
+    description: "Start the slate clean.",
+    useType: "KU",
+    useInfo: "Replaces your entire tank with a fresh set of random fish equal to the previous count.",
+  },
+  frying_pan: {
+    description: "BONK.",
+    useType: "KU",
+    useInfo: "Forces a random pair of fish to fight immediately.",
+  },
+  glue: {
+    description: "Huff?",
+    useType: "SU",
+    useInfo: "Secretly presents a yes-or-no choice; saying yes subtracts 10 power from every fish.",
+  },
+  hyena: {
+    description: "A hyena? FUCK.",
+    useType: "HU",
+    useInfo: "Triggers a hidden fight where the loser is guaranteed to die.",
+  },
+  jam: {
+    description: "Oh. How lovely.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  ketchup: {
+    description: "Tomato sugar juice. Yum.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  light_bulb: {
+    description: "Put it in your mouth?",
+    useType: "HU",
+    useInfo: "Prompts a yes-or-no choice; yes kills one random fish but grants +50 power to another.",
+  },
+  marshmallows: {
+    description: "A soft landing. Just in case.",
+    useType: "HU",
+    useInfo: "Automatically saves the next fish that would die after you receive the item.",
+  },
+  meat: {
+    description: "Oooh carnivorous.",
+    useType: "SU",
+    useInfo: "Secretly grants +10 power to every fish in your tank.",
+  },
+  meerkat: {
+    description: "A meerkat? How'd that get in there?",
+    useType: "KU",
+    useInfo: "Lets you rename the first fish you click after redeeming.",
+  },
+  mustard: {
+    description: "Ewwwwwwwww.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  orca: {
+    description: "NONONONONONO EVERYBODY RUN NONONON---",
+    useType: "KU",
+    useInfo: "Kills every fish but resurrects one with ten times its previous power.",
+  },
+  potatochip: {
+    description: "I'll take a potato chip...",
+    useType: "HU",
+    useInfo: "Secretly lets you assign a new trait to the first fish you click after redeeming.",
+  },
+  rubber_duck: {
+    description: "Honk honk.",
+    useType: "HU",
+    useInfo: "Quietly adds a decorative rubber duck to your tank without other effects.",
+  },
+  salmon: {
+    description: "Mmmm fresh.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  soap: {
+    description: "The goat.",
+    useType: "HU",
+    useInfo: "Renames every fish in the tank to Soap1, Soap2, Soap3, and so on.",
+  },
+  spatula: {
+    description: "Flip 'em.",
+    useType: "HU",
+    useInfo: "Flips a fish's sprite upside down for thirty minutes after redeeming.",
+  },
+  water: {
+    description: "Never hurts to have a little more.",
+    useType: "HU",
+    useInfo: "Quietly does nothing at all.",
+  },
+  wet_wipe: {
+    description: "Lick?",
+    useType: "HU",
+    useInfo: "Shows a yes-or-no prompt; saying yes gives +50 power to a random fish, otherwise nothing happens.",
+  },
+  white_cheese: {
+    description: "Holy fuck that's delicious.",
+    useType: "KU",
+    useInfo: "Reveals the hidden power stat of the first fish you click after redeeming.",
+  },
+  wilddog: {
+    description: "Awwww a pupp--",
+    useType: "HU",
+    useInfo: "Broadcasts a fake message that the player has died without changing anything else.",
+  },
+};
