8fb66d65862e9f97b7a01c4abaf93167a99e63ae
ratioactivity <semmatron@gmail.com>
2025-10-25 23:18:49 -0500

    Update script-fixed.js

--- script-fixed.js
+++ script-fixed.js
@@ -856,6 +856,7 @@ function handleFight(a, b) {
   logEvent(`${winner.name} (${winner.species}) defeated ${loser.name} (${loser.species})!`);
 
   // ✅ Only one reward
+  awardRandomItemForVictory(winner);
   
   winner.wins = (winner.wins || 0) + 1;
   if (winner.wins >= 10) {
